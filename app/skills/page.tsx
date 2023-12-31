3c87b9eaf94a288503d74e02930fe50f67e371ee
Abhishek Bansal <abhishekbansal938@gmail.com>
2023-12-31 15:08:16 +0530

    final commit

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -1,3 +1,5 @@
+import { NextSVG, ReactSVG, ThreeSVG } from "../constants/constants"
+
 export default function Page() {
     return <>
 
@@ -12,9 +14,51 @@ export default function Page() {
                         Skills where i have expertise
                     </div>
                 </div>
+                <div className="flex flex-col gap-4">
+                    <div className="flex flex-col gap-1">
+                        <div className="text-2xl text-gray-300 font-normal">
+                            Website Frontend
+                        </div>
+                        <div className="text-sm font-normal text-gray-500">
+                            Technologies i use to create website frontend
+                        </div>
+                    </div>
+                    <div className="flex flex-row gap-4">
+                        {
+                            frontEndSkills.map((e) => <SkillCard iconClass={e.iconClass} icon={e.icon} desc={e.desc} />)
+                        }
+                    </div>
+                </div>
             </div>
         </div>
 
 
     </>
+}
+
+const frontEndSkills = [
+    {
+        icon: ReactSVG,
+        desc: "",
+        iconClass: ""
+    },
+    {
+        icon: NextSVG,
+        desc: "",
+        iconClass: "fill-white"
+    }, {
+        icon: ThreeSVG,
+        desc: "",
+        iconClass: "stroke-white"
+    }
+]
+
+const SkillCard = ({ icon, desc, iconClass }: { icon: any, desc: string, iconClass: string }) => {
+    return <div className="w-60 h-60 bg-opacity-40 rounded-lg backdrop-blur-lg flex justify-center items-center bg-black p-4">
+
+        <div className={`w-full ${iconClass}`}>
+            {icon}
+        </div>
+
+    </div>
 }
