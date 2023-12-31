fcbbcff21b53d9ff9f45123ec81c0db0e8dd44aa
Abhishek Bansal <abhishekbansal938@gmail.com>
2023-12-31 18:43:58 +0530

    final commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ export default function Page() {
   return <>
 
     <div className="w-full h-full rounded flex flex-col justify-center items-center">
-      <div className="text-8xl text-gray-300 uppercase">
+      <div className="text-3xl md:text-6xl lg:text-8xl text-gray-300 uppercase">
         abhishek bansal
       </div>
-      <div className="text-md text-gray-300 gap-2">
+      <div className="text-xs text-gray-400 gap-2 line-clamp-1 text-ellipsis">
         A Web developer, Android Developer, IOS Developer, ML / AI Expert
       </div>
     </div>
