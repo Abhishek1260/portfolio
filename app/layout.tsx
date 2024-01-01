c40b8652880139494b8b8cf6444a7f683f2fe97d
Abhishek Bansal <abhishekbansal938@gmail.com>
2024-01-01 15:06:46 +0530

    final commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,11 +16,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className='w-screen h-screen'>
-        {/* <div className='-z-10 w-screen h-screen top-0 left-0 fixed blur-sm'> */}
-        <Experience />
-        {/* </div> */}
-        {/* <div className=' z-50 w-screen h-screen flex flex-col overflow-y-hidden'>
+      <body>
+        <div className='-z-10 w-screen h-screen top-0 left-0 fixed blur-sm'>
+          <Experience />
+        </div>
+        <div className=' z-50 w-screen h-screen flex flex-col overflow-y-hidden'>
           <div className='w-full h-24'>
             <Navbar />
           </div>
@@ -30,7 +30,7 @@ export default function RootLayout({
               {children}
             </div>
           </div>
-        </div> */}
+        </div>
       </body>
     </html >
   )
