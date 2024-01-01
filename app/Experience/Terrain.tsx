dd07d75e706e671fd0297ba35f775432ea8858c9
Abhishek Bansal <abhishekbansal938@gmail.com>
2024-01-01 14:59:17 +0530

    fianl commit

--- app/Experience/Terrain.tsx
+++ app/Experience/Terrain.tsx
@@ -38,6 +38,7 @@ export default function Terrain({ instance }: { instance: any }) {
             side={DoubleSide}
             ref={refer}
             uniforms={uniforms}
+            wireframe={true}
             vertexShader={
                 `
             vec4 permute(vec4 x){return mod(((x*34.0)+1.0)*x, 289.0);}
